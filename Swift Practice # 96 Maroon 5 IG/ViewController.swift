be241d2d589b720fb01b289e774c1ff20f47c0f7
Dogpa <dogpa0130@gmail.com>
2021-10-17 20:48:13 +0800

    Initial Commit

---
+++ Swift Practice # 96 Maroon 5 IG/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift Practice # 96 Maroon 5 IG
+//
+//  Created by Dogpa's MBAir M1 on 2021/10/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
